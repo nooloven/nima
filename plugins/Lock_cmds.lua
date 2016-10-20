95fec3c64b6640ba810fa607a5ca2c66e3e5666f
nooloven <nima.shirdel1987@gmail.com>
2016-10-20 16:50:47 +0330

    Add files via upload

---
+++ plugins/Lock_cmds.lua
@@ -0,0 +1,33 @@
+-- @Ernest_TM
+kicktable = {}
+local function run(msg, matches)
+if msg.to.type == 'channel' then
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['cmds'] then
+                lock_cmds = data[tostring(msg.to.id)]['settings']['cmds']
+            end
+        end
+    end
+    local channel = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_cmds == "yes" then
+        send_large_msg(channel, '')
+        delete_msg(msg.id, ok_cb, false)
+    end
+end
+ end
+return {
+  patterns = {
+    "(/(.+))",
+ "(!(.+))",
+ "(#(.+))",
+
+ },
+  run = run
+}
+-- @Ernest_TM
